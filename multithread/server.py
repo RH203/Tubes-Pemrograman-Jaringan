c22b462cc46deb3c793205ca5c63b29f9b165d3d
SandRune <khristianalexsandro@gmail.com>
2024-06-09 17:10:14 +0700

    Added 4MB max file and more

--- multithread/server.py
+++ multithread/server.py
@@ -36,62 +36,60 @@ class ChatServer:
           self.remove_client(c)
 
   def handle_client(self, client):
-    '''
-    Fungsi ini akan menangani komunikasi antara server dan client, menerima pesan dari client, memprosesnya, dan menyebarkannya.
+      while True:
+        try:
+          message = client.recv(1024)
+          if b'\n' in message:
+            header, file_data = message.split(b'\n', 1)
+            header = header.decode('ascii')
+            nickname, file_info = header.split(': ', 1)
+            file_type, file_name, file_size = file_info.split(' ', 2)
+            file_size = int(file_size)
 
-    Parameter:
-        - client: Socket client yang sedang aktif.
-    '''
-    while True:
-      try:
-        message = client.recv(1024)
-        if b'\n' in message:
-          header, file_data = message.split(b'\n', 1)
-          header = header.decode('ascii')
-          nickname, file_info = header.split(': ', 1)
-          file_type, file_name, file_size = file_info.split(' ', 2)
-          file_size = int(file_size)
+            if file_size > 4 * 1024 * 1024:  # 4MB
+              self.broadcast("File size exceeds the maximum limit of 4MB.".encode('ascii'))
+              return
 
-          remaining_data = file_size - len(file_data)
-          while remaining_data > 0:
-            chunk = client.recv(min(remaining_data, 1024))
-            file_data += chunk
-            remaining_data -= len(chunk)
+            remaining_data = file_size - len(file_data)
+            while remaining_data > 0:
+              chunk = client.recv(min(remaining_data, 1024))
+              file_data += chunk
+              remaining_data -= len(chunk)
 
-          self.save_file(file_data, file_type, file_name)
-          self.broadcast(f"{nickname} sent a {file_type} file: {file_name}".encode('ascii'), client)
-        else:
-          self.broadcast(message, client)
-      except:
-        self.remove_client(client)
-        break
+            self.save_file(file_data, file_type, file_name)
+            self.broadcast(f"{nickname} sent a {file_type} file: {file_name}".encode('ascii'), client)
+          else:
+            self.broadcast(message, client)
+        except:
+          self.remove_client(client)
+          break
 
   def save_file(self, file_data, file_type, file_name):
-    '''
-    Fungsi ini akan menyimpan file yang diterima dari client ke direktori yang sesuai di server, terdapat condition untuk
-    menghindari konflik nama jika ada.
+      '''
+      Fungsi ini akan menyimpan file yang diterima dari client ke direktori yang sesuai di server, terdapat condition untuk
+      menghindari konflik nama jika ada.
 
-    Parameter:
-        - file_data: Data file yang diterima dari client.
-        - file_type: Jenis file yang diterima (audio, image, atau video).
-        - file_name: Nama file yang diterima dari client.
-    '''
-    script_dir = os.path.dirname(__file__)
-    uploads_dir = os.path.join(script_dir, '..', 'uploads', file_type)
-    os.makedirs(uploads_dir, exist_ok=True)
-    base_name, extension = os.path.splitext(file_name)
-    file_path = os.path.join(uploads_dir, file_name)
+      Parameter:
+          - file_data: Data file yang diterima dari client.
+          - file_type: Jenis file yang diterima (audio, image, atau video).
+          - file_name: Nama file yang diterima dari client.
+      '''
+      script_dir = os.path.dirname(__file__)
+      uploads_dir = os.path.join(script_dir, '..', 'uploads', file_type)
+      os.makedirs(uploads_dir, exist_ok=True)
+      base_name, extension = os.path.splitext(file_name)
+      file_path = os.path.join(uploads_dir, file_name)
 
-    index = 1
-    while os.path.exists(file_path):
-      new_file_name = f"{base_name}({index}){extension}"
-      file_path = os.path.join(uploads_dir, new_file_name)
-      index += 1
+      index = 1
+      while os.path.exists(file_path):
+        new_file_name = f"{base_name}({index}){extension}"
+        file_path = os.path.join(uploads_dir, new_file_name)
+        index += 1
 
-    with open(file_path, 'wb') as file:
-      file.write(file_data)
+      with open(file_path, 'wb') as file:
+        file.write(file_data)
 
-    print(f"Saved {file_type} file: {os.path.basename(file_path)}")
+      print(f"Saved {file_type} file: {os.path.basename(file_path)}")
 
   def remove_client(self, client):
     '''
@@ -116,7 +114,7 @@ class ChatServer:
     while True:
       client, address = self.server.accept()
       print(f'Connected with {str(address)}')
-
+      
       client.send('NICK'.encode('ascii'))
       nickname = client.recv(1024).decode('ascii')
       self.nicknames.append(nickname)
